850aca055575d2068d041a98e8b8e40d23409ca8
Joe DeCello <Joe_DeCello@symantec.com>
2014-08-21 15:26:36 +0000

    new

--- masterless/modules/example/manifests/init.pp
+++ masterless/modules/example/manifests/init.pp
@@ -5,24 +5,35 @@
 # Example module - used for testing masterless wrapper
 
 class example (
-  param1 = 1,
-  param2 = 2,
-  expath = '/tmp',
-{
+  $param1 = 1,
+  $param2 = 2,
+  $expath = '/tmp',
+) {
   notice(">>> example class <<<")
   notify{"Loading example class": }
   tag("example")
 
   ##FILES
   file { 'example_file':
-    path   => "$expath/example",
-    sourc  => "puppet:///modules/example/example",
+    ensure  => present,
+    path   => "$expath/example_file",
+    source => "puppet:///modules/example/example",
+    mode    => '0644',
     backup => ".$::timestamp",
   }
 
-  exec { "install_example_default_enable":
-    command => 'sed -i "s/^startup=0$/startup=1/" /etc/default/example',
-    onlyif => 'grep -c ^startup=0$ /etc/default/example',
+  file { 'example_template':
+    ensure  => present,
+    path   => "$expath/example_template",
+    content => template('example/example.erb'),
+    mode    => '0644',
+    backup => ".$::timestamp",
   }
 
+  exec { "install_example_file_via_exec":
+    command => 'echo "example" > /tmp/example_exec',
+    unless => 'grep -c ^example /tmp/example_exec',
+  }
+
+} # end of class
 #######################################################
